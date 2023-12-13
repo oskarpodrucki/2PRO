0a9a28cc2e5a675aa8d4ef15ff5aa6c63bc2f657
Oskar Podrucki <oskar.podrucki1@gmail.com>
2023-12-13 22:03:36 +0100

    ...

--- 6.PHP/php3/kolory/kolory.php
+++ 6.PHP/php3/kolory/kolory.php
@@ -4,24 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kolory - wynik</title>
-    <link rel="stylesheet" href="style.css">
+    <style>
+    
+    <?php
+    
+    $color = $_POST['color1'];
+    $text = $_POST['color2'];
+
+    ?>
+
+
+    p {
+            height: 50%;
+            width: 50%;
+            margin: 50px auto;
+            border: 2px dashed red;
+            color: <?php echo $text; ?>;
+            background-color: <?php echo $color; ?>;
+    }
+    </style>
+   
 </head>
 <body>
     
     <?php
     
-    
-    $color = $_POST['color1'];
-    $text = $_POST['color2'];
-
     echo $color."<br>";
     echo $text."<br>";
 
 
-
-
+    echo '<p>Oskar Podrucki</p>'
 
     ?>
 
 </body>
 </html>
+
